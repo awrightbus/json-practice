7639aad6061ca651ea14af19cb15f49d4cdb38fa
awright <awrightbus@gmail.com>
2021-06-07 14:00:19 -0500

    replace interactice character hard coded values and  make sure only show classes if student

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as unparsedJson from '../../json/data.json';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+  // vars used by the about modal:
+  closeModal: string;
+  // vars related to data retrieval and storage:
+  dataRetrievalComplete: Boolean = false;
+  interactiveCharacters: any = {};
+  nonInteractiveCharacters: any = {};
+  // vars used by bonus challenges (initialized, but currently unused):
+  harrysGlassesAreRepaired: Boolean = false;
+  voldemortIsCensored: Boolean = false;
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
+
+  ngOnInit(): void {
+    let parsedJson = JSON.parse(JSON.stringify(unparsedJson));
+    let parsedCharacters = parsedJson.default.characters;
+    Object.keys(parsedCharacters).forEach((key: string) => {
+      if (key === "harry_potter" || key === "lord_voldemort") {
+        this.interactiveCharacters[key] = parsedCharacters[key];
+      } else {
+        this.nonInteractiveCharacters[key] = parsedCharacters[key];
+      }
+    });
+    this.dataRetrievalComplete = true;
+
+
+    console.log(this.interactiveCharacters.harry_potter.student_classlist.core_classes,'interactive characters console log');
+  }
+
+  // The triggerModal & getDismissReason functions are related to the about modal:
+  // (documented at: https://www.remotestack.io/angular-bootstrap-modal-popup-tutorial-example/)
+  triggerModal(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+      this.closeModal = `Closed with: ${res}`;
+    }, (res) => {
+      this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+}
